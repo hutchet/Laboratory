44c7dc55b0d9eab550ae89f5cf717e9afe64a60a
hutchet <okashi1993@gmail.com>
2026-07-17 16:40:13 +0700

    Update auth.ts

--- lib/auth.ts
+++ lib/auth.ts
@@ -10,6 +10,16 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   pages: { signIn: "/login" },
+  callbacks: {
+    jwt({ token, user }) {
+      if (user) token.id = user.id
+      return token
+    },
+    session({ session, token }) {
+      if (session.user) session.user.id = token.id as string
+      return session
+    },
+  },
   providers: [
     Credentials({
       credentials: {
