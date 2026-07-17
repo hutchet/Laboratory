b7b1387a840c0409451bc8271c29cc7ae330ff64
hutchet <okashi1993@gmail.com>
2026-07-17 15:30:44 +0700

    Add files via upload

---
+++ app/login/page.tsx
@@ -0,0 +1,9 @@
+// TODO (Buoc 2): dung form nay goi signIn("credentials", { email, password }) tu "@/lib/auth"
+export default function LoginPage() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>Dang nhap TaskFlow</h1>
+      <p>Form dang nhap se duoc dung sau khi cai dat next-auth va ket noi database.</p>
+    </main>
+  )
+}
